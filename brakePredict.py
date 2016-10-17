f94cef705d3c063e8a1ecbeb24dca04e19fb8625
werowe <werowe@walkerrowe.com>
2016-10-17 16:51:51 -0300

    Add files via upload

---
+++ brakePredict.py
@@ -0,0 +1,37 @@
+import mist
+import os
+import numpy as np
+import pandas as pd
+import math
+from numpy import array
+from pyspark.mllib.regression import LabeledPoint
+from pyspark.mllib.classification import LogisticRegressionModel
+
+class Predict:
+ 
+
+    def __init__(self, job):
+        job.sendResult(self.runModel(job))
+
+    def runModel(self, job):
+        val = job.parameters.values()
+        list = val.head()
+        size = list.size()
+        pylist = []
+        count = 0
+        while count < size:
+            pylist.append(list.head())
+            count = count + 1
+            list = list.tail()
+
+
+        heat = pylist[0]
+        km = pylist[1]
+        lrm = LogisticRegressionModel.load(job.sc, "/tmp/brakeModel")
+        worn = lrm.predict([heat, km])
+        return ("brake is worn=", worn)
+      
+
+predict = Predict(mist.Job())
+
+ 
